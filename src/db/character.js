c0c186d8d283fa00f24308f326c22cd7129770ae
Matti Vakkilainen <matti.vakkilainen@yle.fi>
2018-12-26 23:31:41 +0200

    use levenshtein in postgres

--- src/db/character.js
+++ src/db/character.js
@@ -10,27 +10,33 @@ const searchCharacters = async (name, realm) => {
 
   const result = await query(
     `SELECT
-      c.name, c.level, c.race, c.class, NULLIF(CONCAT_WS('/', tree1_pts, tree2_pts, tree3_pts), '') AS spec, c.sex, c.inspected_at AS seen,
+      c.name, c.level, c.race, c.class,
+      NULLIF(CONCAT_WS('/', tree1_pts, tree2_pts, tree3_pts), '') AS spec,
+      c.sex, c.inspected_at AS seen,
       r.name AS realm,
-      g.name as guild
+      g.name AS guild,
+      levenshtein(LOWER(c.name), $1) AS distance
     FROM character c
     LEFT JOIN realm r ON c.realm_id = r.id
     LEFT JOIN guild g ON c.guild_id = g.id
-    WHERE LOWER(c.name) LIKE $1
-    ${(realmName && 'AND LOWER(r.name) = $2') || ''}
+    ${(realmName && 'WHERE LOWER(r.name) = $2') || ''}
+    ORDER BY distance ASC
     LIMIT ${MAX_SEARCH_RESULTS}`,
     realmName ? ['%' + characterName + '%', realmName] : ['%' + characterName + '%']
   )
 
-  return _(result.rows)
-    .orderBy(['level', character => levenshtein.get(character.name, name)], ['desc', 'asc'])
-    .value()
+  return result.rows
+  // return _(result.rows)
+  //   .orderBy(['level', character => levenshtein.get(character.name, name)], ['desc', 'asc'])
+  //   .value()
 }
 
 const getCharacter = async (name, realm) => {
   try {
     const result = await query(
-      `SELECT c.id, c.name, c.level, c.race, c.class, NULLIF(CONCAT_WS('/', tree1_pts, tree2_pts, tree3_pts), '') AS spec, c.sex, c.inspected_at AS seen,
+      `SELECT c.id, c.name, c.level, c.race, c.class,
+        NULLIF(CONCAT_WS('/', tree1_pts, tree2_pts, tree3_pts), '') AS spec,
+        c.sex, c.inspected_at AS seen,
         r.name AS realm,
         g.name as guild
       FROM character c
